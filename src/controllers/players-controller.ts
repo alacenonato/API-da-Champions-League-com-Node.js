c72b5992cf3698c7446ee82e5ccb18ef000e8ae9
Alace Pereira Nonato <alac_pereira@hotmail.com>
2024-11-29 13:40:30 -0300

    Criando a Camada de Service

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
-export const getPlayer = (req: Request, res: Response) => {
-    res.status(200).json({ player: "Ronaldo" });
+import { getPlayerService } from '../services/player-service';
+export const getPlayer = async (req: Request, res: Response) => {
+    
+    const data = await getPlayerService();
+    
+    res.status(200).json(data);
 
 };
