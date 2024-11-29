94d74a78a89118c73fc9f17838ef3c4fbf3a876a
Alace Pereira Nonato <alac_pereira@hotmail.com>
2024-11-29 15:57:40 -0300

    Get player by Id

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from 'express';
-import { getPlayerService } from '../services/player-service';
+import * as service from '../services/player-service';
 import { ok } from '../utils/http-helper';
 
 export const getPlayer = async (req: Request, res: Response) => {
-    
-    const httpResponse = await getPlayerService();
 
-            
+    const httpResponse = await service.getPlayerService();
     res.status(httpResponse.statusCode).json(httpResponse.body);
 
 };
+
+
+export const getPlayerById = async (req: Request, res: Response) => {
+ 
+    const id = parseInt(req.params.id)
+    const httpResponse = await service.getPlayerByIdService(id)
+    res.status(httpResponse.statusCode).json(httpResponse.body);
+}
