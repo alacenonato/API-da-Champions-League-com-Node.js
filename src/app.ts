50360a9768caa778b851683fa4742060b0716cb6
Alace Pereira Nonato <alac_pereira@hotmail.com>
2024-11-30 16:40:18 -0300

    Finalizando o projeto Com Felipão

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { json } from 'express';
 import express from 'express';
 import router from './routers/routers';
+import cors from 'cors';
 
 function createApp() {
     const app = express();
@@ -8,7 +9,16 @@ function createApp() {
     app.use(json());
 
     app.use('/api', router);
-   
+
+    // uso do cors
+    // const corsOpitions = {
+    //     origing: "http://alace.systme.com",
+    //     methods: ['GET'],
+    // }
+    // app.use(cors(corsOpitions));
+
+    app.use(cors());
+
     return app;
 }
 
